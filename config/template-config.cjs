89cfe7b2c72b2d1f36a184c486fbb25bb76fe64b
saberdante <53332443+saberdante@users.noreply.github.com>
2022-11-08 09:40:50 +0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -60,6 +60,49 @@ const TEMPLATE_CONFIG = [
       失败用户: {{fail_post_ids.DATA}}
     `
   },
+   {
+    id: '0003',
+    title: '## 又是元气满满的一天！ Tiezi！',
+    desc: `
+    
+      ![](https://mmbiz.qpic.cn/mmbiz_gif/rcAjicibQf6ScaNse2GhHQU1o01eeOrAbjQQ6kicTpgDia6vkCe4DH5lVwVgibMrGZFG02vqeumiao9wBahvzRxU1EZA/0?wx_fmt=gif)
+      
+      **{{tian_api_morning_greeting.DATA}}**
+      
+      今天是**{{date.DATA}}**
+      
+      不用看日历了：{{holidaytts.DATA}}
+      
+      ---
+      
+      下面为您播报**{{city.DATA}}**今天的天气 o(〃'▽'〃)o 
+      
+      **{{city.DATA}}**今天的天气是：  {{weather.DATA}}
+      
+      气温(最高/最低):  {{max_temperature.DATA}} / {{min_temperature.DATA}}
+      
+      今日降雨量： {{tian_api_weather_pcpn_0.DATA}}
+      
+      预防感冒提醒： {{ganmao.DATA}}
+      
+      牛牛解忧杂货店铺温馨提示： {{tian_api_weather_tips_0.DATA}}
+      
+      ---
+   
+      {{course_schedule.DATA}}
+      
+      ---
+      
+      顺便说一下：{{birthday_message.DATA}}
+      
+      ---
+      
+      {{note_en.DATA}}
+      
+      {{note_ch.DATA}}
+   
+    `
+  },
 ]
 
 module.exports = TEMPLATE_CONFIG
