dabd7dfe12e701e51ea2100fcd98aca466f76657
saberdante <53332443+saberdante@users.noreply.github.com>
2022-11-14 21:57:45 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -111,7 +111,7 @@ const USER_CONFIG = {
       name: '动物园',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       // 使用其他通道时，请严格按照各个通道的教程进行填写
-      id: 'PDU17598T87M4VpjtvYAPK2wHtc6tq4Qrn5oBnSU6',
+      id: 'PDU17598T40BbCuS8C1XHD44MQHtT3OyMV4RXNAwz',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       // 使用其他通道时，请填写 config/template-config.cjs 中某个想要使用的模板的id
       useTemplateId: '0001',
